dcd03a4c49d638e8169bf3a7e0712bd6f0463f49
XaXaTuSHeCH <eg03or@mail.ru>
2025-02-09 17:25:07 +0700

    Task1_ping

---
+++ Task1/ping.py
@@ -0,0 +1,32 @@
+import subprocess
+
+import csv
+
+addresses = [
+    "discordapp.com",
+    "wikipedia.com",
+    "microsoft.com",
+    "example.com",
+    "yandex.com",
+    "google.com",
+    "github.com",
+    "apple.com",
+    "gmail.com",
+    "vk.com",
+]
+with open("ping.csv", "w") as file:
+    dw = csv.DictWriter(
+        file,
+        delimiter=",",
+        fieldnames=["address", "ping (ms)"],
+    )
+    dw.writeheader()
+    writer = csv.writer(file)
+    for address in addresses:
+        result = subprocess.run(["ping", "-c", "1", address], capture_output=True)
+        if result.returncode == 0:
+            rtt = result.stdout.decode()
+            writer.writerow([address, rtt[rtt.find("time=") + 5 : rtt.find("ms")]])
+        else:
+            writer.writerow([address, "no connection"])
+    file.close()
