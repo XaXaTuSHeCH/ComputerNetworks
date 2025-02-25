f59ff8824a909307f57ea14597e80d9d7d94a3fc
XaXaTuSHeCH <eg03or@mail.ru>
2025-02-26 00:33:13 +0700

    Task3 update

--- Task3/Parser.py
+++ Task3/Parser.py
@@ -7,6 +7,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 
+
 try:
     with open("data.json", "r") as file:
         creds = json.load(file)
@@ -47,6 +48,7 @@ try:
     )
     continue_button.click()
     time.sleep(1)
+
     passwd_input = WebDriverWait(driver, 15).until(
         EC.presence_of_element_located((By.XPATH, "//input[@name='password']"))
     )
@@ -57,49 +59,62 @@ try:
     )
     enter_button.click()
     time.sleep(1)
+
     search_button = WebDriverWait(driver, 15).until(
         EC.element_to_be_clickable((By.XPATH, "//a[@data-mark='FiltersSearchButton']"))
     )
     search_button.click()
 
-    WebDriverWait(driver, 15).until(
-        EC.presence_of_element_located(
-            (By.XPATH, "//article[@data-name='CardComponent']")
-        )
-    )
-
-    flats = driver.find_elements(By.XPATH, "//article[@data-name='CardComponent']")
     data = []
 
-    for flat in flats:
+    for _ in range(5):
+        time.sleep(3)
+        WebDriverWait(driver, 15).until(
+            EC.presence_of_element_located(
+                (By.XPATH, "//article[@data-name='CardComponent']")
+            )
+        )
+
+        flats = driver.find_elements(By.XPATH, "//article[@data-name='CardComponent']")
+
+        for flat in flats:
+            try:
+                link = flat.find_element(By.XPATH, ".//a[@href]").get_attribute("href")
+                title_elements = flat.find_elements(
+                    By.XPATH, ".//span[@data-mark='OfferTitle']"
+                )
+                subtitle_elements = flat.find_elements(
+                    By.XPATH, ".//span[@data-mark='OfferSubtitle']"
+                )
+                price_element = flat.find_element(
+                    By.XPATH, ".//span[@data-mark='MainPrice']"
+                )
+                address_elements = flat.find_elements(
+                    By.XPATH, ".//a[@data-name='GeoLabel']"
+                )
+
+                title = (
+                    " ".join([elem.text for elem in title_elements])
+                    + " "
+                    + " ".join([elem.text for elem in subtitle_elements])
+                )
+                price = price_element.text
+                address = ", ".join([elem.text for elem in address_elements])
+
+                data.append([title, price, address, link])
+            except Exception as e:
+                print(f"Ошибка при обработке объявления: {e}")
+
         try:
-            link = flat.find_element(By.XPATH, ".//a[@href]")
-            link_url = link.get_attribute("href")
+            driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+            time.sleep(1)
 
-            title_elements = flat.find_elements(
-                By.XPATH, ".//span[@data-mark='OfferTitle']"
+            next_button = WebDriverWait(driver, 5).until(
+                EC.element_to_be_clickable((By.XPATH, "//a[span[text()='Дальше']]"))
             )
-            subtitle_elements = flat.find_elements(
-                By.XPATH, ".//span[@data-mark='OfferSubtitle']"
-            )
-            price_elements = flat.find_element(
-                By.XPATH, ".//span[@data-mark='MainPrice']"
-            )
-            address_elements = flat.find_elements(
-                By.XPATH, ".//a[@data-name='GeoLabel']"
-            )
-
-            title = (
-                " ".join([elem.text for elem in title_elements])
-                + " "
-                + " ".join([elem.text for elem in subtitle_elements])
-            )
-            price = price_elements.text
-            address = ", ".join([elem.text for elem in address_elements])
-
-            data.append([title, price, address, link_url])
+            next_button.click()
         except Exception as e:
-            print(f"Ошибка при обработке объявления: {e}")
+            print(f"Ошибка при переходе на следующую страницу: {e}")
 
     with open("flats.csv", "w", newline="", encoding="utf-8") as file:
         writer = csv.writer(file, delimiter="|")
